9f97655150cd737487d03f01abdfc87cc3a3da54
赵东亮 <3012287667@qq.com>
2016-09-07 16:18:03 +0800

    5. 滚动collectionView的过程中实现数据加载
    1. 模拟网络加载新的数据

--- waterFall/ViewController.m
+++ waterFall/ViewController.m
@@ -65,6 +65,41 @@
 
 }
 
+
+#pragma mark - 滚动过程中
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+
+    // 如果footerView还没有出现，或者说正在加载数据，什么也不做直接返回
+    if (self.footerView == nil || self.footerView.activityVIew.isAnimating == YES) {
+        return ;
+    }
+
+    
+    // footerView完全出现之后才去加载更多
+    if (self.collectionView.contentOffset.y + self.collectionView.bounds.size.width > CGRectGetMaxY(self.footerView.bounds)) {
+        
+        // 菊花开始转动
+        [self.footerView.activityVIew startAnimating];
+        
+        // 这是加载的本地数据，所以会很快显示
+        // 模拟网络状况
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            
+            // 1. 加载数据
+            
+            // 2. 刷新表格
+            
+            // 3. 菊花停止转动
+            
+            // 4. 将footerView设置为nil
+            
+        });
+    }
+
+}
+
+
+
 #pragma mark - 懒加载
 - (NSMutableArray *)shops {
 
